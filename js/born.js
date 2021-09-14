06e27254a73adae41530a42481170fd7e0f549f2
findsmyfriends <kriengkrai.born@gmail.com>
2021-09-14 19:07:04 +0700

    Update

--- js/born.js
+++ js/born.js
@@ -6,6 +6,7 @@ function myFunction() {
 
 function showPage() {
   document.getElementById("loader").style.display = "none";
+  document.getElementById("titleName").style.display = "none";
   document.getElementById("myDiv").style.display = "block";
 }
 
